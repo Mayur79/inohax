17039bf1943df28d14e139ea1a219d8e773fd54d
Mayur Bhoyar <mayurbhoyar4455@gmail.com>
2024-10-24 16:00:14 +0530

    conflictsolved

--- src/components/sections/navbar.tsx
+++ src/components/sections/navbar.tsx
@@ -37,7 +37,6 @@ export const Navbar = () => {
         <>
             {isMounted && (
                 <header className="shadow-inner backdrop-blur-3xl bg-black/40 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-gray-900/45 z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
-                <header className="shadow-inner backdrop-blur-3xl bg-black/40 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-gray-900/45 z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
                     <Link href="/" className="font-bold text-lg flex items-center">
                         Inohax 1.0
                     </Link>
